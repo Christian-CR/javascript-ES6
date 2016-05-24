370dce6dc3c2db7d07473dbe059d0f890ac51da8
VodooPeople <cristian.vodoopeople@gmail.com>
2016-05-23 21:28:29 -0600

    listo

---
+++ scriptMethods.js
@@ -0,0 +1,19 @@
+window.onload = fuction(){
+var yousay = "goodbye";
+
+if (yousay.startsWith("goodbye")){
+var isay = "hello";
+}
+
+console.log('you say ${yousay}, I say ${isay}');
+
+/* NEW STRINGS METHODS
+
+- replace
+- startsWith
+- endsWith
+- includes
+
+*/
+
+}
