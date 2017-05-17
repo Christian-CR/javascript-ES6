d05fb446c2410731d7adedc1688596de568a9e81
vodoo <VodooPeople@users.noreply.github.com>
2017-05-16 21:31:19 -0600

    Se agregó método Map()

--- scriptMethods.js
+++ scriptMethods.js
@@ -22,5 +22,15 @@ console.log('you say ${yousay}, I say ${isay}');
   var lenguajes2 = ["ruby", "javascript", "go", "php"];
   
   
+  // Map o Mapa
   
+  var car = new Map();
+  
+  car.set("brand", "Volkswagen");
+  car.set("name", "yeta");
+  car.set("color", "black");
+  
+  console.log(car.get("brand")); // retorna Volkswagen
+  console.log(car.has("brand")); // pregunta si existe brand
+  car.delete("brand"); // borra el valor
  
