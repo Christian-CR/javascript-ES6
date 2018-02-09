87a46de782e73e0c54355c330ff3e4efd3633e7e
vodoo <VodooPeople@users.noreply.github.com>
2018-02-08 22:22:11 -0600

    create vat

--- use_of_map.js
+++ use_of_map.js
@@ -1,3 +1,11 @@
 
 // ejemplo usando la funcion map
 
+var animals = [
+  { name: 'Fluffykins', species: 'rabbit' },
+  { name: 'Caro',       species: 'dog' },
+  { name: 'Hamilton',   species: 'dog' },
+  { name: 'Harold', species: 'fish' },
+  { name: 'Ursula', species: 'cat' },
+  { name: 'Jummy', species: 'fish' }
+]
