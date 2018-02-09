6c8b25741ae1f009fef99ce808524aaf894c291d
vodoo <VodooPeople@users.noreply.github.com>
2018-02-08 23:23:53 -0600

    const dragon events :D

--- arrow_function_and_const.js
+++ arrow_function_and_const.js
@@ -3,6 +3,17 @@ const dragonEvents = [
   { type: 'attack', value: 12, target: 'player-dorkman' },
   { type: 'yawn', value: 40 },
   { type: 'eat', target: 'horse' },
-  { type: 'attack', value: , target: '' },
-  { type: 'attack', value: '', target: '' }
+  { type: 'attack', value: 23, target: 'player-fluffykins' },
+  { type: 'attack', value: 12, target: 'player-dorkman' }
 ]
+
+const totalDamageOnDorkman = dragonEvents
+.filter(function(event){
+  return event.type === 'attack' 
+})
+
+// retorna todos los type que tengan el string attack
+
+console.log( 'totalDamageOnDorkman', totalDamageOnDorkman );
+
+// tomado del video: arroe function in javascript - what, why and how
