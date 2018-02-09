44a3777db4773bf7c1161253482c8b21a54c0051
vodoo <VodooPeople@users.noreply.github.com>
2018-02-08 23:14:31 -0600

    Update arrow_function_and_const.js

--- arrow_function_and_const.js
+++ arrow_function_and_const.js
@@ -1,1 +1,8 @@
 
+const dragonEvents = [
+  { type: 'attack', value: 12, target: 'player-dorkman' },
+  { type: 'yawn', value: 40 },
+  { type: 'eat', target: 'horse' },
+  { type: 'attack', value: , target: '' },
+  { type: 'attack', value: '', target: '' }
+]
