134c89594dac86852c1cd62fedaec2cfeb4822e2
vodoo <VodooPeople@users.noreply.github.com>
2018-02-10 13:31:14 -0600

    Update filter_function.js

--- filter_function.js
+++ filter_function.js
@@ -1,2 +1,26 @@
 
 
+var animals = [
+  {name:'Fluffkins', species:'rabbit'},
+  {name: 'Caro', species: 'dog'},
+  {name: 'Hamilton', species: 'dog'},
+  {name: 'Harold', species: 'fish'},
+  {name: 'Ursula', species: 'cat'},
+  {name: 'jimmy', species: 'fish'}
+]
+
+//******* modo normal:
+
+var dogs = []
+for (var i=0; i<animals.lenght; i++){
+  if (animals[i] === 'dog'){
+        dogs.push(animals[i])
+      }
+}
+
+
+// ******** usando filter
+
+var dogs = animals.filter(function(){
+  return ainimal.species === 'dog'
+})
