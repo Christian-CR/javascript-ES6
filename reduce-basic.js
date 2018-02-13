b1756da005292c842362979403aa0c0da65b0148
vodoo <VodooPeople@users.noreply.github.com>
2018-02-13 07:35:06 -0600

    Update reduce-basic.js

--- reduce-basic.js
+++ reduce-basic.js
@@ -1,1 +1,24 @@
 
+// sumar todos los valores
+
+var orders = [
+  {amount: 250},
+  {amount: 400},
+  {amount: 100},
+  {amount: 325}
+]
+
+// utilizando foor loop
+
+var totalAmount = 0;
+for (var i=0; i<orders.lenght; i++){
+  totalAmount += orders[i].amount
+}
+
+console.log(totalAmount) 
+
+
+// Utilizando reduce
+
+var totalAmount = orders
+.reduce((sum, order) => sum + order.amount, 0)
