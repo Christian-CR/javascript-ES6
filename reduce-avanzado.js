0f17d223b0a741c54af538a86d19c6c7fa9b4f3d
vodoo <VodooPeople@users.noreply.github.com>
2018-02-13 07:45:13 -0600

    Update reduce-avanzado.js

--- reduce-avanzado.js
+++ reduce-avanzado.js
@@ -1,1 +1,13 @@
 
+// portiendo de esta lista de un archivo externo llamado data.txt  separado por tabs
+
+mark  johansson waffle  iron  80  2 
+mark  johansson blender 200 1
+mark  johansson knife 10  4
+Nikita  Smith waffle  iron  80  1
+Nikita  Smith knife 10  2
+Nikita  Smith pot 20  3
+
+
+
+
