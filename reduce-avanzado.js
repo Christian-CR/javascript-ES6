8b1ec30c008b5a6a895335953e01ca9ff3dd9319
vodoo <VodooPeople@users.noreply.github.com>
2018-02-13 21:34:42 -0600

    Update reduce-avanzado.js

--- reduce-avanzado.js
+++ reduce-avanzado.js
@@ -67,7 +67,21 @@ output [
 ]
 */
 
+// para crear un JSON
+  .trim()
+  .split('\n')
+  .map(line => line.split('\t'))
+  .reduce((customers, line) => {
+    customers[line[0]] = customers[line[0]] || []
+    customers[line[0]].push({
+      name: line[1],
+      price: line[2],
+      quantity: line[3]
+    })
+  return customers
+  }, {})
 
+console.log('output', JSON.stringify(output, null, 2))
 
 
 
