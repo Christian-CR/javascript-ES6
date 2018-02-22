ef7a11e122617f86c86b9cf25e6b63247b86016c
vodoo <VodooPeople@users.noreply.github.com>
2018-02-22 13:39:40 -0600

    ejemplo mas básico

--- prototype-basics.js
+++ prototype-basics.js
@@ -3,3 +3,29 @@ function talk(){
   console.log(this.sound)
 }
 
+let animal ={
+ talk 
+}
+
+let cat = {
+  sound: 'meow'
+}
+
+let dog = {
+  sound: 'woof'
+}
+
+let prarieDog = {
+  howl: function(){
+    console.log(this.sound.toUpperCase())
+  }
+}
+
+Object.setPrototypeOf(cat, animal)
+Object.setPrototypeOf(dog, animal)
+cat.talk()
+dog.talk()
+Object.setPrototypeOf(prarieDog, dog)
+prarieDog.howl()
+
+// tomado del video:  Prototype basics - Object Creation in JavaScript P3
