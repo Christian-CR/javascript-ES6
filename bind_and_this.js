5b44a55d0c15bdc9c6ff093e1592585f5c683426
vodoo <VodooPeople@users.noreply.github.com>
2018-02-26 13:25:47 -0600

    Ejemplo de uso de bind y this

---
+++ bind_and_this.js
@@ -0,0 +1,27 @@
+
+let dog = {
+  sound: 'woof'.
+  talk: function(){
+    console.log(this.sound)
+  }
+}
+
+dog.talk() // retorna  'woof'
+let talkFunction = dog.talk
+talkFunction() // retorna  undefined
+
+// otra forma
+
+
+dog.talk() // retorna  'woof'
+let talkFunction = dog.talk
+let boundFunction = talkFunction.bind(dog)  // el método bind enlaza la otra funcion
+boundFunction() // retorna  'woof'
+
+
+// let button = document.getElementsById('myNiceButton')
+
+button.addEventListener(
+  'click',
+  dog.talk.bind(dog)
+)
